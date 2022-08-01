d6f5a77e5f90f86a72173feb1b562a3c1afae719
Epsy0 <serrano.lucas@gmail.com>
2022-08-01 12:28:46 +0200

    premiere étape login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,25 +1,68 @@
-import { Button, Text, View } from "react-native";
-//TODO/ removencjdsnfj
-//*jrqkrejnfrn
-//
+import React, { useState } from "react";
+import { View, ImageBackground, StyleSheet, Text } from "react-native";
+import { Button, Icon, Input } from "@rneui/themed";
+import { connect } from "react-redux";
 
-export default function MapScreen(props) {
+function LoginScreen(props) {
+  const [signinEmail, setSigninEmail] = useState('');
+  const [signinPwd, setSigninPwd] = useState('');
+
+  var handleSubmitSignIn = async () => {
+    var res = await fetch('http://172.16.189.141:3000/users/sign-in', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: `email=${signinEmail}&pwd=${signinPwd}`
+    })
+    res = await res.json()
+    if (res.saved) {
+      props.navigation.navigate('Home')
+    }
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: "#e67e22",
-      }}
-    >
-      <Text>Hello Cunt</Text>
-      <Button
-        title="Go to Map"
-        onPress={() => {
-          props.navigation.navigate("Home");
-        }}
-      />
+    <View style={styles.container}>
+        <View style={{ width: "70%" }}>
+          <Input
+            className="Login-input"
+            onChangeText={(user) => setSigninEmail(user)}
+            value={signinEmail}
+            placeholder="john@lacapsule.com"
+          />
+        </View>
+        <View style={{ width: "70%" }}>
+          <Input
+            secureTextEntry= {true}
+            className="Login-input"
+            onChangeText={(email) => setSigninPwd(email)}
+            value={signinPwd}
+            leftIcon={{ type: "font-awesome", name: "user", color: "#eb4d4b" }}
+            placeholder="Password"
+          />
+        </View>
+        <Button
+          type="solid"
+          onPress={()=> handleSubmitSignIn()}
+        >
+          <Icon name="chevron-right" color="red" />
+          Confirmer
+        </Button>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default (LoginScreen);
